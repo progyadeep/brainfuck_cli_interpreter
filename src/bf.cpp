b4432c3ad7abbcff9bb460850fefce0d2dd52112
Progyadeep Moulik <progyadeep@outlook.com>
2020-10-17 20:36:26 +0530

    Removed the use of "preprocess.h" file
    Instead of iterating over the input string, the input is now fetched character by character, validating every token as it's extracted from stdin.

--- src/bf.cpp
+++ src/bf.cpp
@@ -1,81 +1,109 @@
 #include <iostream>
 #include <cstdlib>
-#include "preprocess.h"
+#include <cstdio>
 using namespace std;
 
 union Cell{
     signed char c;
 };
 
-union Cell* ptr;
-union Cell* start;
+union Cell *ptr;
+union Cell *start;
 
-void process(string);
+void process (string);
+string getInput (void);
 
-int main(){
-    ptr = (union Cell*)calloc(30000, sizeof(union Cell));
+int main (){
+    ptr = (union Cell *) calloc (30000, sizeof (union Cell));
     start = ptr;
-	cout << "*** ";
-	cin >> inpMain;
-	while(inpMain[0] != '#'){
-		inpMain = preprocess(inpMain);
-		//checking for fucked up input:
-		if(inpMain != "IT")
-			process(inpMain);
+    string inpMain = getInput ();
+    
+    while (inpMain != "#"){
+        if (inpMain != "!" && inpMain != "")
+	        process (inpMain);
 
-		//on to next cycle:	
-		cout << "\n*** ";
-		cin >> inpMain;
-	}
-	
-	free(start);
-	return 0;
+        //on to next cycle:
+        inpMain = getInput ();
+    }
+
+    free (start);
+    return 0;
 }
 
-void process(string inp){
-	for(int i=0; i<inp.length(); i++){
-		switch(inp[i]){
-			case '[':
-				int e,c;
-				e = i+1; c=0;
-				while(inp[e] != ']' || c!=0){
-					if(inp[e] == '[')
-						c++;
-					else if(inp[e] == ']')
-						c--;
-					e++;
-				}
-				while(ptr->c != 0){
-					process(inp.substr(i+1, e-i-1));
-				}
-				i=e;
-			break;
-			case '+':
-				ptr->c++;
-			break;
-			case '-':
-				ptr->c--;
-			break;
-			case '>':
-				ptr++;
-			break;
-			case '<':
-				ptr--;
-			break;
-			case '.':
-				cout << ptr->c;
-			break;
-			case ',':
-        		signed char tmp;
-         		cin >> tmp;
-				ptr->c = (signed char)tmp;
-			break;
-			case '$':
-				free(start);
-				ptr = (union Cell*)calloc(30000, sizeof(union Cell));
-			break;
-			default:
-				cout << "IDK What to do now\n";
-		}
-	}
+string getInput (){
+    cout << "\n*** ";
+    string str = "";
+    char ch;
+    long int index = 0l;
+    while ((ch = getchar ()) != '\n'){
+        if (ch != ' '){
+	        if (ch == '.' || ch == ',' || ch == '+' || ch == '-' || ch == '<'|| ch == '>' || ch == '[' || ch == ']'){
+	            str += ch;
+	        }
+	        else if (ch == '#'){
+	            str="#";
+	            break;
+	        }
+	        else if(ch == '$'){
+	            str="$";
+	            break;
+	        }
+	        else{
+	            cout << "Invalid token \"" << ch << "\" at index: " << index;
+	            str = "!";
+	            getchar(); //swallowing the '\n'
+	            break;
+	        }
+	        index++;
+	    }
+    }
+
+    return str;
+}
+
+void process (string inp){
+    for (int i = 0; i < inp.length (); i++){
+        switch (inp[i]){
+	        case '[':
+	            int e, c;
+	            e = i + 1;
+	            c = 0;
+	            while (inp[e] != ']' || c != 0){
+	                if (inp[e] == '[')
+		                c++;
+	                else if (inp[e] == ']')
+		                c--;
+	                e++;
+	            }
+	            while (ptr->c != 0){
+	                process (inp.substr (i + 1, e - i - 1));
+	            }
+	            i = e;
+	        break;
+	        case '+':
+	            ptr->c++;
+	        break;
+	        case '-':
+	            ptr->c--;
+	        break;
+	        case '>':
+	            ptr++;
+	        break;
+	        case '<':
+	            ptr--;
+	        break;
+	        case '.':
+	            cout << ptr->c;
+	        break;
+	        case ',':
+	            ptr->c = (signed char) getchar();
+	            getchar(); //swallowing the '\n'
+	        break;
+	        default:
+	            free (start);
+	            ptr = (union Cell *) calloc (30000, sizeof (union Cell));
+	            start = ptr;
+	            getchar(); //swallowing the '\n'
+        }
+    }
 }
